4d7647b106847851a4b160134e3c6b541faffb81
Raymond Zhou <somecrazyidiot@gmail.com>
2013-04-29 01:11:47 -0400

    Editing text now actually changes the underlying model.

--- app/controllers/cardCtrl.js
+++ app/controllers/cardCtrl.js
@@ -17,6 +17,10 @@ kanbanBit.directive( 'board', function() {
   };
 });
 
+kanbanBit.controller( 'CardCtrl', [ '$scope', function( $scope ) {
+  console.log( $scope.card.text );
+}]);
+
 kanbanBit.directive( 'card', function() {
   return {
     restrict: 'C',
@@ -25,16 +29,41 @@ kanbanBit.directive( 'card', function() {
     link: function( scope, element, attrs ) {
       element.bind( 'click', function() {
         console.log( 'card: ' + element.attr( 'card' ) + ', ' + element.attr( 'board' ) );
+        console.log( scope.card.text );
       });
     }
   };
 });
 
-kanbanBit.directive( 'click', function() {
-  return function( scope, element ) {
-    element.bind( 'click', function() {
-      element.attr( 'contenteditable', 'true' );
-    });
+// Content editable code taken from http://docs.angularjs.org/api/ng.directive:ngModel.NgModelController.
+kanbanBit.directive( 'editable', function() {
+  return {
+    require: '?ngModel',
+
+    link: function( scope, element, attrs, ngModel ) {
+      element.bind( 'click', function() {
+        element.attr( 'contenteditable', 'true' );
+      });
+
+      // Do nothing if no ng-model exists.
+      if ( !ngModel ) { return; }
+
+      ngModel.$render = function() {
+        element.html( ngModel.$viewValue || '' );
+      };
+
+      // Update on change.
+      element.bind( 'blur keyup change', function() {
+        scope.$apply( read );
+      });
+
+      // Initialize element to current value.
+      element.html( ngModel.$viewValue );
+
+      function read() {
+        ngModel.$setViewValue( element.html() );
+      }
+    }
   };
 });
 
@@ -47,11 +76,19 @@ kanbanBit.directive( 'enter', function() {
   };
 });
 
-
 kanbanBit.directive( 'leave', function() {
   return function( scope, element, attrs ) {
     element.bind( 'mouseleave', function() {
       element.removeClass( attrs.enter );
+    });
+  };
+});
+
+kanbanBit.directive( 'debug', function() {
+  return function( scope, element, attrs ) {
+    element.bind( 'click', function() {
+      console.log( 'hello' );
+      console.log( scope.boards[0].cards[ scope.boards[0].cards.length - 1 ].text );
     });
   };
 });
